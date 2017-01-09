46fec32b6f568ab8f39b48bad16d8044f8e91d16
RB <rb.rana1993@gmail.com>
2017-01-09 14:53:35 +0530

    accepting port from Heroku CLI

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"log"
+
 	"net/http"
+	"os"
 )
 
 func main() {
-	log.Println("Listening on port 3000")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":"+port, nil)
 
 }
