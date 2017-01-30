e59707d03cbeb0c422a1ea4367068ccb7af7e92a
sourabhbhagat <sbhagat@tekion.com>
2017-01-30 23:30:41 +0530

    added Possible queries for calculateMonGoQuery function

--- main.go
+++ main.go
@@ -8,8 +8,42 @@ import (
 	"fmt"
 )
 
+
+func calculateMonGoQuery(dbName,dbQuery string) (string, error){
+
+	//Possible Queries
+	//1. Inserting Documents
+	//2. Finding Documents
+	//3. Finding Documents using Operators
+	    //3.1 $gt / $gte || $lt / $lte - greater than / greater than equals , lesser than / lesser than equals
+	    //3.2 $exists - does an attribute exist or not
+	    //3.3 $regex - Perl-style pattern matching
+	    //3.4 $type - search by type of an element
+
+	//4. Updating Documents
+	//5. Removing Documents
+	//6. Working with Indexes
+	//7. Pipeline Stages
+	    //7.1 $project
+	    //7.2 $match
+	    //7.3 $group
+	    //7.4 $sort
+	    //7.5 $skip
+	    //7.6 $limit
+	    //7.7 $unwind
+	//8. Aggregation Expressions
+	    //8.1 $sum
+	    //8.2 $avg
+	    //8.3 $min / $max
+	    //8.4 $push
+	    //8.5 $addToSet
+	    //8.6 $first / $last
+
+}
+
 func Convert(w http.ResponseWriter, r *http.Request) {
-	var dbQuery string
+	var dbName,dbQuery,generateButton string
+
 	if r.Method == "GET" {
 
 		http.ServeFile(w, r, "index.html")
@@ -18,7 +52,7 @@ func Convert(w http.ResponseWriter, r *http.Request) {
 		r.ParseMultipartForm(32 << 20)
 		//validation :  len(r.Form["username"][0])
 
-		dbName := r.FormValue("dbQuery")
+		dbName = r.FormValue("dbQuery")
 		if len(dbName) == 0 {
 			fmt.Fprint(w, "dbName cannot be empty")
 			os.Exit(0)
@@ -29,14 +63,24 @@ func Convert(w http.ResponseWriter, r *http.Request) {
 
 	} else {
 		//POST : generate button action
+		generateButton = r.FormValue("generate")
 
+		//check if button is pressed
+		if len(generateButton) != 0 {
 
+			ans,err := calculateMonGoQuery(dbName,dbQuery)
+			if err != nil {
+				log.Println("calculateMonGoQuery Error", err.Error())
+			}
+
+			fmt.Fprint(w , ans)
+		} else {
+			fmt.Fprint(w , "generatebutton is not pressed!")
+		}
 	}
-
-
 }
-func main() {
 
+func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "9876"
