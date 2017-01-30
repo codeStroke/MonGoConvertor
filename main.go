1456813679ea3e3491560b16e3f4e7867cd791d7
sourabhbhagat <sbhagat@tekion.com>
2017-01-30 17:55:36 +0530

    added form input processing, modified index.html

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
-
 	"net/http"
 	"os"
-
+	_ "expvar"
+	"log"
+	"fmt"
 )
 
+func Convert(w http.ResponseWriter, r *http.Request) {
+	var dbQuery string
+	if r.Method == "GET" {
+
+		http.ServeFile(w, r, "index.html")
+
+		// Form submitted
+		r.ParseMultipartForm(32 << 20)
+		//validation :  len(r.Form["username"][0])
+
+		dbName := r.FormValue("dbQuery")
+		if len(dbName) == 0 {
+			fmt.Fprint(w, "dbName cannot be empty")
+			os.Exit(0)
+		}
+		dbQuery = r.FormValue("dbQuery")
+		fmt.Fprint(w , "Entered query is  :", dbQuery)
+
+
+	} else {
+		//POST : generate button action
+
+
+	}
+
+
+}
 func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8000"
+		port = "9876"
 	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
+	http.HandleFunc("/", Convert)
+	log.Println("Listening....")
 	http.ListenAndServe(":"+port, nil)
-
 }
